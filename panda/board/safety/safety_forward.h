8a7f32375a5464c06632c98255f427357c82f033
xx979xx <40252818+xx979xx@users.noreply.github.com>
2019-07-21 18:44:24 +0300

    reset function

--- panda/board/safety/safety_forward.h
+++ panda/board/safety/safety_forward.h
@@ -1,6 +1,7 @@
 
 // Stores the array index of a matched car fingerprint/forwarding profile
 int enabled = -1;
+int hyundai_camera_cnt = 1;
 
 
 static void forward_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
@@ -8,15 +9,24 @@ static void forward_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
   int bus = GET_BUS(to_push);
   int addr = GET_ADDR(to_push);
   
+  // Rest everything if forwarding stopped and 3 camera msgs did not show in car bus
+  if ((enabled = 0) && (hyundai_camera_cnt >= 3)) {
+    hyundai_camera_detected = 0;
+    hyundai_giraffe_switch_2 = 0;
+    hyundai_camera_cnt = 1;
+  }
+  
   if ((bus == 0) && (addr == 832)) {
     hyundai_camera_detected = 1;
+    hyundai_camera_cnt -= 1;
   }
   
   // Find out which bus the camera is on
   if ((bus != 0) && (addr == 832)) {
     hyundai_camera_bus = bus;
+    hyundai_camera_cnt += 1;
   }
-  
+
   // 832 is lkas cmd. If it is on camera bus, then giraffe switch 2 is high
   if ((addr == 832) && (bus == hyundai_camera_bus) && (hyundai_camera_detected != 1)) {
     hyundai_giraffe_switch_2 = 1;
@@ -25,12 +35,12 @@ static void forward_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
     safety_cb_enable_all();
     // begin forwarding with that profile
     enabled = 1;
-    }
+  }
   if ((enabled == 1) && (hyundai_camera_detected == 1)) {
     safety_cb_disable_all();
     // camera connected, disable forwarding
     enabled = 0;
-    }
+  }
 
 }
 
